ef4e179033363f5ec12e725d3d0e76c8921e2ec6
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-04-11 15:50:55 +0800

    Update Navbar.zh.js

--- src/components/LocalizedNavbar/Navbar.zh.js
+++ src/components/LocalizedNavbar/Navbar.zh.js
@@ -11,28 +11,6 @@ const zhNavbar = {
       label: "ChatGPT 使用说明",
       position: "left",
     },
-    {
-      to: "https://nav.newzone.top",
-      label: "开源工具导航",
-      position: "left",
-    },
-    {
-      href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
-      label: 'Find on Product Hunt',
-      target: '_blank',
-      position: 'left',
-    },
-    { type: "localeDropdown", position: "right" },
-    {
-      href: "https://github.com/rockbenben/ChatGPT-Shortcut",
-      position: "right",
-      className: "header-github-link",
-    },
-    {
-      href: "https://discord.gg/PZTQfJ4GjX",
-      position: "right",
-      className: "header-discord-link",
-    },
   ],
 };
 

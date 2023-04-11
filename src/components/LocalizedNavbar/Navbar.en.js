e1b12f7e0c682221027e84f88d60fe57546283da
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-04-11 15:47:06 +0800

    Update Navbar.en.js

--- src/components/LocalizedNavbar/Navbar.en.js
+++ src/components/LocalizedNavbar/Navbar.en.js
@@ -1,4 +1,4 @@
-const enNavbar = {
+const zhNavbar = {
   hideOnScroll: true,
   title: "ChatGPT SC",
   logo: {
@@ -7,11 +7,33 @@ const enNavbar = {
   },
   items: [
     {
-      to: " ",
-      label: " ",
+      to: "https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html",
+      label: "ChatGPT 使用说明",
       position: "left",
+    },
+    {
+      to: "https://nav.newzone.top",
+      label: "开源工具导航",
+      position: "left",
+    },
+    {
+      href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
+      label: 'Find on Product Hunt',
+      target: '_blank',
+      position: 'left',
+    },
+    { type: "localeDropdown", position: "right" },
+    {
+      href: "https://github.com/rockbenben/ChatGPT-Shortcut",
+      position: "right",
+      className: "header-github-link",
+    },
+    {
+      href: "https://discord.gg/PZTQfJ4GjX",
+      position: "right",
+      className: "header-discord-link",
     },
   ],
 };
 
-module.exports = enNavbar;
+module.exports = zhNavbar;
